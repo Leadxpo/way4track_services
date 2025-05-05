698711a5f212efe0f83f1afdf930e3c7cfb453bb
Teja Kola <kolateja09@gmail.com>
2025-05-05 12:46:54 +0530

    @new

--- src/client/entity/client.entity.ts
+++ src/client/entity/client.entity.ts
@@ -19,7 +19,7 @@ export class ClientEntity extends BaseEntity {
     @PrimaryGeneratedColumn({ name: 'id' })
     id: number;
 
-    @Column({ name: 'name', type: 'varchar', length: 100 })
+    @Column({ name: 'name', type: 'varchar', length: 100, nullable: true })
     name: string;
 
     @Column({ name: 'phone_number', type: 'varchar', length: 15, unique: true })
