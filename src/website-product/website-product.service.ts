93e3f0b0c0a3e1f79e801244fed08b0c459d3b7b
Teja Kola <kolateja09@gmail.com>
2025-05-10 20:25:05 +0530

    g

--- src/website-product/website-product.service.ts
+++ src/website-product/website-product.service.ts
@@ -163,10 +163,10 @@ export class WebsiteProductService {
         try {
             const items = await this.websiteProductRepository
                 .createQueryBuilder('product')
-                .leftJoinAndSelect('product.device', 'device')
-                .leftJoinAndSelect('product.amenities', 'amenities')
-                .leftJoinAndSelect('product.application', 'application')
-                .leftJoinAndSelect('product.productApp', 'productApp')
+                // .leftJoinAndSelect('product.device', 'device')
+                // .leftJoinAndSelect('product.amenities', 'amenities')
+                // .leftJoinAndSelect('product.application', 'application')
+                // .leftJoinAndSelect('product.productApp', 'productApp')
                 // .leftJoinAndSelect('product.Blog', 'Blog')
                 .where('product.companyCode = :companyCode AND product.unitCode = :unitCode', {
                     companyCode: req.companyCode,
