73d64b703067b92f7582fdc204d79e87afe43b81
Leadxpo <teamleadxpo@gmail.com>
2025-05-17 13:25:14 +0530

    solution

--- src/website-product/website-product.controller.ts
+++ src/website-product/website-product.controller.ts
@@ -39,6 +39,7 @@ export class WebsiteProductController {
         { name: "blogImage", maxCount: 1 },
         { name: "productIcon", maxCount: 1 },
         { name: "chooseImage", maxCount: 1 },
+        { name: "solutionImage", maxCount: 1 },
       ],
       multerOptions
     )
@@ -55,6 +56,8 @@ export class WebsiteProductController {
       blogImage?: Express.Multer.File[];
       productIcon?: Express.Multer.File[];
       chooseImage?: Express.Multer.File[];
+      solutionImage?: Express.Multer.File[];
+
     }
   ): Promise<CommonResponse> {
     console.log(dto, "//////");
