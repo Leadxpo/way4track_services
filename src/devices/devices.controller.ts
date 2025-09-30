f3b2a1f9e63e810243965e0ca1f0da4a29e6490e
jatin <jatinjayadev@gmail.com>
2025-09-30 21:04:56 +0530

    updated issue with deleting existing files in device

--- src/devices/devices.controller.ts
+++ src/devices/devices.controller.ts
@@ -30,6 +30,7 @@ export class DeviceController {
         }
     ): Promise<CommonResponse> {
         try {
+            console.log("Received dto:::", dto);
             if (dto.id) dto.id = Number(dto.id);
             if (dto.isRelay) dto.isRelay = Boolean(dto.isRelay);
             if (dto.relayAmt) dto.relayAmt = Number(dto.relayAmt);
@@ -46,7 +47,7 @@ export class DeviceController {
                 points.push({
                     title: dto[`points[${i}].title`] || '',
                     desc: dto[`points[${i}].desc`] || dto[`points[${i}].dest`] || '',
-                    file: '' // Will be set after upload
+                    file: dto[`points[${i}].file`] || '',
                 });
                 i++;
             }
